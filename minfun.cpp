1a6fcd7806de2749eb958180a4b4d2e07c0a4a14
Aakash <93485049+Aakash-sittu@users.noreply.github.com>
2022-08-22 22:29:50 +0530

    Maximum of 3 Numbers

---
+++ minfun.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void minfun(int a, int b, int c)
+{
+    int t;
+    if (a > b)
+        t = a;
+    else
+        t = b;
+    if (t > c)
+        cout << t << " is bigger." << endl;
+    else
+        cout << c << " is bigger." << endl;
+}
+int main(int argc, char const *argv[])
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    minfun(a, b, c);
+    cout << endl;
+    return 0;
+}
